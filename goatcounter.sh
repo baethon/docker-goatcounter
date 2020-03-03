ec7d4d3771aff2ec9e8d9ecc34e3fc156c42cb8e
Radoslaw Mejer <radmen@baethon.com>
2020-03-03 15:23:04 +0100

    Attach some options only when available

--- goatcounter.sh
+++ goatcounter.sh
@@ -1,11 +1,21 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-set -eu
+set -e
 
-goatcounter serve \
-  -automigrate \
-  -listen "$GOATCOUNTER_LISTEN" \
-  -db "$GOATCOUNTER_DB" \
-  -tls none \
-  -smtp "smtp://$GOATCOUNTER_SMTP" \
-  -auth "email:$GOATCOUNTER_EMAIL"
+declare OPTS=""
+
+OPTS="$OPTS -automigrate"
+OPTS="$OPTS -listen $GOATCOUNTER_LISTEN"
+OPTS="$OPTS -tls none"
+OPTS="$OPTS -auth email:$GOATCOUNTER_EMAIL"
+OPTS="$OPTS -db $GOATCOUNTER_DB"
+
+if [ -n "$GOATCOUNTER_SMTP" ]; then
+  OPTS="$OPTS -smtp smtp://$GOATCOUNTER_SMTP"
+fi
+
+if [ -n "$GOATCOUNTER_DEBUG" ]; then
+  OPTS="$OPTS -debug all"
+fi
+
+goatcounter serve $OPTS
