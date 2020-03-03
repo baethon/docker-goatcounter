ff1daa42f6171fa7a4d82e8a5e9227ceddb8a69c
Radoslaw Mejer <radmen@baethon.com>
2020-03-03 22:05:12 +0100

    WIP: add Dockerfile for v1.0.*
    It's not working yet. The entrypoint.sh uses commands which are not
    available in v1.0.0. I should wait for a new release before pushing this
    to master.

---
+++ v1.0/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -eu
+
+migrate_db ()
+{
+  goatcounter migrate \
+    -createdb \
+    -db "$GOATCOUNTER_DB" \
+    all
+}
+
+create_site ()
+{
+  goatcounter create \
+    -domain "$GOATCOUNTER_DOMAIN" \
+    -email "$GOATCOUNTER_EMAIL" \
+    -db "$GOATCOUNTER_DB"
+}
+
+migrate_db
+
+if ! create_site; then
+  # stupid way to silence the errors created by the command
+  # we should ignore only 'zdb.TX fn: cname: already exists.'
+  /bin/true
+fi
+
+exec "$@"
