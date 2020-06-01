be410303d6278f3b217112fdfe069afacf8958d0
Radoslaw Mejer <radmen@baethon.com>
2020-06-01 12:23:14 +0200

    Add v1.3

---
+++ v1.3/goatcounter.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+declare OPTS=""
+
+OPTS="$OPTS -listen $GOATCOUNTER_LISTEN"
+OPTS="$OPTS -tls none"
+OPTS="$OPTS -email-from $GOATCOUNTER_EMAIL"
+OPTS="$OPTS -db $GOATCOUNTER_DB"
+
+if [ -n "$GOATCOUNTER_SMTP" ]; then
+  OPTS="$OPTS -smtp $GOATCOUNTER_SMTP"
+fi
+
+if [ -n "$GOATCOUNTER_DEBUG" ]; then
+  OPTS="$OPTS -debug all"
+fi
+
+goatcounter serve $OPTS
